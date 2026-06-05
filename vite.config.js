c3a020f6e38692dff7f6c2d3b6cca7023e51f8a1
Babi0706 <dandoluchathurvedareddy@gmail.com>
2026-06-05 22:21:09 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,24 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Smart-Attendance-System/', // GitHub repository name
+
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg', 'models/*.json', 'models/*.shard*'],
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'mask-icon.svg',
+        'models/*.json',
+        'models/*.shard*'
+      ],
       manifest: {
         name: 'Smart Attend Blockchain System',
         short_name: 'SmartAttend',
-        description: 'Secure, Decentralized Attendance using Blockchain and Biometrics.',
+        description:
+          'Secure, Decentralized Attendance using Blockchain and Biometrics.',
         theme_color: '#00e5ff',
         background_color: '#020d1a',
         display: 'standalone',
@@ -40,7 +49,7 @@ export default defineConfig({
               cacheName: 'google-fonts-cache',
               expiration: {
                 maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+                maxAgeSeconds: 60 * 60 * 24 * 365
               },
               cacheableResponse: {
                 statuses: [0, 200]
@@ -54,7 +63,7 @@ export default defineConfig({
               cacheName: 'gstatic-fonts-cache',
               expiration: {
                 maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+                maxAgeSeconds: 60 * 60 * 24 * 365
               },
               cacheableResponse: {
                 statuses: [0, 200]
@@ -64,5 +73,5 @@ export default defineConfig({
         ]
       }
     })
-  ],
+  ]
 })
